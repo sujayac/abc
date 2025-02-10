1dee1f0bf7332835c4b0ef921543163c0527505f
sujayac <107612038+sujayac@users.noreply.github.com>
2025-02-10 23:22:05 +0530

    Update BookingServiceImpl.java

--- BookingServiceImpl.java
+++ BookingServiceImpl.java
@@ -4,7 +4,6 @@ class BookingServiceImpl{
     ZonedDateTime todaysDate = System.now().getZonedDateTime();
     while(bookingRequest.getParticipationDate > todaysDate){
       if(class.getMembersEnrolled.size() <= class.getMaxCapacity){
-
         List<Member> membersEnrolled = class.getMembersEnrolled().stream().filter(member -> member.getId() == bookingRequest.getMemberId()).collect(Collectors.toList());
         if(membersEnrolled.contains(bookingRequest.getMemberId){
           // log message - member already enrolled in requested class
