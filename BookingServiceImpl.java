4c0886e42064dcbf887b252217101f1551536708
sujayac <107612038+sujayac@users.noreply.github.com>
2025-02-10 23:14:16 +0530

    Update BookingServiceImpl.java

--- BookingServiceImpl.java
+++ BookingServiceImpl.java
@@ -1,6 +1,16 @@
 class BookingServiceImpl{
 
   private boolean bookMyClass(Member member, Class class, BookingRequest bookingRequest){
-    
+    ZonedDateTime todaysDate = System.now().getZonedDateTime();
+    while(bookingRequest.getParticipationDate > todaysDate){
+      if(class.getMembersEnrolled.size() <= class.getMaxCapacity){
+
+        class.getMembersEnrolled().stream().filter(member -> member.getId() != bookingRequest.getMemberId())
+        return true;
+      }else{
+        // log message - Class already reached maximum capacity
+        return false;
+      }
+    }
   
 }
