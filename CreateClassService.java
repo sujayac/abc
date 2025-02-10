8fae8d4b29ed758f767af08790f36f64b97c839f
sujayac <107612038+sujayac@users.noreply.github.com>
2025-02-10 23:33:54 +0530

    Create CreateClassService.java

---
+++ CreateClassService.java
@@ -0,0 +1,10 @@
+class CreateClassService{
+
+ 
+  private boolean createClassService(String name, ZonedDateTime startDate, ZonedDateTime endDate, int duration, int maxCapacity){
+    Class clubClass = new Class(name, startDate, endDate, duration, maxCapacity);
+    
+    
+  }
+  
+}
