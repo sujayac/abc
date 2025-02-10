77e2fa5f365d94c81b0f165f1d08f8355561d67c
sujayac <107612038+sujayac@users.noreply.github.com>
2025-02-10 23:21:52 +0530

    Update BookingServiceImpl.java

--- BookingServiceImpl.java
+++ BookingServiceImpl.java
@@ -5,12 +5,17 @@ class BookingServiceImpl{
     while(bookingRequest.getParticipationDate > todaysDate){
       if(class.getMembersEnrolled.size() <= class.getMaxCapacity){
 
-        class.getMembersEnrolled().stream().filter(member -> member.getId() != bookingRequest.getMemberId())
-        return true;
+        List<Member> membersEnrolled = class.getMembersEnrolled().stream().filter(member -> member.getId() == bookingRequest.getMemberId()).collect(Collectors.toList());
+        if(membersEnrolled.contains(bookingRequest.getMemberId){
+          // log message - member already enrolled in requested class
+          return false;
+        }else{
+          class.getMembersEnrolled.add(member);
+          return true;
+        }
       }else{
         // log message - Class already reached maximum capacity
         return false;
       }
     }
-  
 }
