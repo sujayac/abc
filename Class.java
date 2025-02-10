ed3f57b38e4740953eec32a08c8276f52b97c41c
sujayac <107612038+sujayac@users.noreply.github.com>
2025-02-10 23:36:11 +0530

    Update Class.java

--- Class.java
+++ Class.java
@@ -8,7 +8,11 @@
     private int maxCapacity;
     private List<member> membersEnrolled;
 
-    public class(){
-    
+    public class(String name, ZonedDateTime startDate, ZonedDateTime endDate, int duration, int maxCapacity){
+      this.name = name;
+      this.startDate = startDate;
+      this.endDate = endDate;
+      this.duration = duration;
+      this.maxCapacity = maxCapacity;
     }
 }
